b226e4a03fa64ef4eb900d0bfad835e532223111
Hoang Le <kuhpro@gmail.com>
2021-01-14 21:33:49 -0600

    Add UserRepository

---
+++ src/main/java/io/huyhoang/instagramclone/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package io.huyhoang.instagramclone.repository;
+
+import io.huyhoang.instagramclone.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
